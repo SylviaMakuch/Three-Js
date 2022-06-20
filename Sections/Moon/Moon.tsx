b3f8064d2cc8f9cb27bc0ad36a7ab85cca0315bf
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-06-19 20:32:15 -0600

    req inline style sheet

--- Sections/Moon/Moon.tsx
+++ Sections/Moon/Moon.tsx
@@ -9,21 +9,21 @@ const Moon = (props) => {
     const [hovered, setHover] = useState(false)
     const [active, setActive] = useState(false)
     const ref = useRef()
-    useFrame((state, delta) => (ref.current.rotation.y += 0.10))
+    useFrame((state, delta) => (ref.current.rotation.x += 0.05))
+    // useFrame((state, delta) => (ref.current.rotation.y += 0.05))
+    // useFrame((state, delta) => (ref.current.rotation.z += 0.05))
     const texture1 = useLoader(TextureLoader, moon);
 
     return (
-        <group ref={ref} position={[-20, -10, 50]}>
+        <group ref={ref} position={[-40, -10, 90]}>
             <mesh 
                {...props}
                scale={active ? 1.5 : 1}
                onClick={(event) => setActive(!active)}
                onPointerOver={(event) => setHover(true)}
                onPointerOut={(event) => setHover(false)}
-               rotation={[0.05, 0.075, 0]}
                >
-                <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-                <sphereGeometry args={[3, 32, 32]} />
+                <sphereGeometry args={[32, 32, 32]} />
                 <meshStandardMaterial map={texture1} roughness={0.3}/>
             </mesh>
         </group>
